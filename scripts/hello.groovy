25e8e40f7f51632cc2faded0090efb3d65d783eb
Pete <pete.bankhead@gmail.com>
2024-10-08 15:08:16 +0100

    Overdue update for QuPath v0.5.1

--- scripts/hello.groovy
+++ scripts/hello.groovy
@@ -2,6 +2,6 @@
  * Minimal script that should run within QuPath.
  */
 
-import qupath.lib.gui.dialogs.Dialogs
+import qupath.fx.dialogs.Dialogs
 
 Dialogs.showMessageDialog("Greetings", "Hello from my script!")
